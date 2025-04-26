d88a8dc98fe29d8ab29850e73146450b5e415267
Yunus Emre Danabas <yunusdanabas@sabanciuniv.edu>
2025-04-26 18:53:47 +0300

    updated

--- hand_steer_sim/model/keypoint_classifier/__init__.py
+++ hand_steer_sim/model/keypoint_classifier/__init__.py
@@ -1,6 +1,3 @@
-# hand_steer_sim/model/keypoint_classifier/__init__.py
-
 from .keypoint_classifier import KeyPointClassifier
 
 __all__ = ["KeyPointClassifier"]
-# __all__ = ["KeyPointClassifier", "PointHistoryClassifier"]
